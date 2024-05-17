0025b65eca237d210f38a3966b59d23488966c66
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-05-17 09:13:02 +0100

    req 02

--- src/routes/getTalkers.js
+++ src/routes/getTalkers.js
@@ -3,15 +3,23 @@ const fs = require('fs').promises;
 
 const router = express.Router();
 
-// Função para ler o arquivo
 async function getTalkers() {
-  const data = await fs.readFile('./src/talker.json', 'utf-8'); // Corrigindo o caminho do arquivo JSON
+  const data = await fs.readFile('./src/talker.json', 'utf-8');
   return JSON.parse(data);
 }
 
-router.get('/', async (req, res) => { // A rota não precisa de '/talker', pois já é definida ao usar app.use('/talker', talkerRouter);
+router.get('/', async (req, res) => { 
+  console.log('Rota GET /talker acessada');
   const talkers = await getTalkers();
   res.status(200).json(talkers);
 });
 
-module.exports = router; // Corrigindo 'module. Exports' para 'module.exports'
+router.get('/:id', async (req, res) => { 
+  const { id } = req.params;
+  const talkers = await getTalkers();
+  const talker = talkers.find((t) => t.id === parseInt(id, 10));
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(talker);
+});
+
+module.exports = router; 
